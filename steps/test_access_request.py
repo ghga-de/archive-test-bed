bb6544946e56fbddb2663ade10ad86cf0dbe939c
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2023-07-10 18:10:22 +0000

    Fix typos

--- steps/test_access_request.py
+++ steps/test_access_request.py
@@ -13,8 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-"""Step definitions for work package tests"""
+"""Step definitions for access request tests"""
 
 from datetime import timedelta
 from typing import Sequence
