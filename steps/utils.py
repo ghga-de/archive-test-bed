83c1fedaba42999f147f65e28965817d69719ad4
Seyit Zor <seyitzor@gmail.com>
2023-07-11 13:39:30 +0000

    fix incompatible type

--- steps/utils.py
+++ steps/utils.py
@@ -75,7 +75,7 @@ def upload_config_as_file(config: Config, file_metadata_dir: Path):
         "s3_access_key_id": config.s3_access_key_id,
         "s3_secret_access_key": config.s3_secret_access_key.get_secret_value(),
         "bucket_id": config.staging_bucket,
-        "part_size": 1024,
+        "part_size": str(1024),
         "output_dir": str(file_metadata_dir),
     }
 
