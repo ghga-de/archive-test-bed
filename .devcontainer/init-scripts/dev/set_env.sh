5657abf2a41d7bed9296a8afb356d764c24292d6
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2023-07-12 14:54:01 +0000

    Add well-known value service

--- .devcontainer/init-scripts/dev/set_env.sh
+++ .devcontainer/init-scripts/dev/set_env.sh
@@ -26,19 +26,32 @@ if [ -s $KEYS ]; then
 else
   echo "Creating new testing keys..."
   if ! docker run --rm \
-      ghga/auth-km-jobs:main generate-test-keys --num-jwk=2 \
+      ghga/auth-km-jobs:main generate-test-keys --num-jwk=2 --num-c4gh=2 \
       > $KEYS; then
     echo "Error: Testing keys could not be created."
     exit 1
   fi
 fi
-if ! grep -q "JWK_1_PRIV=" $KEYS; then
+if ! grep -q "JWK_1_PRIV=" $KEYS ||
+   ! grep -q "C4GH_1_PRIV=" $KEYS ||
+   ! grep -q "TOKEN=" $KEYS; then
   echo "Error: Keys have not been properly created."
   exit 1
 fi
 
 sed -n "s/^JWK_1_PRIV=/AUTH_KEY=/p" $KEYS > auth.env
 sed -n 's/^TOKEN=\(.*\)/SIMPLE_TOKEN="\1"/p' $KEYS >> auth.env
+
+sed -n "s/^C4GH_1_PUB=/WKVS_CRYPT4GH_PUBLIC_KEY=/p" $KEYS > wkvs.env
+
+sed -n 's/^TOKEN=\(.*\)/FIS_TOKEN="\1"/p' $KEYS > fis.env
+sed -n 's/^TOKEN_HASH=\(.*\)/FIS_TOKEN_HASHES=["\1"]/p' $KEYS >> fis.env
+sed -n "s/^C4GH_2_PRIV=/FIS_PRIVATE_KEY=/p" $KEYS >> fis.env
+
+sed -n "s/^C4GH_2_PUB=/TB_FILE_INGEST_PUBKEY=/p" $KEYS > tb.env
+
+sed -n "s/^C4GH_1_PRIV=/EKSS_SERVER_PRIVATE_KEY=/p" $KEYS > ekss.env
+sed -n "s/^C4GH_1_PUB=/EKSS_SERVER_PUBLIC_KEY=/p" $KEYS > ekss.env
 
 sed -n "s/^JWK_1_PUB=/AUTH_SERVICE_AUTH_KEY=/p" $KEYS > crs.env
 
@@ -48,15 +61,5 @@ sed -n "s/^JWK_1_PUB=/WPS_AUTH_KEY=/p" $KEYS > wps.env
 sed -n "s/^JWK_2_PRIV=/WPS_WORK_PACKAGE_SIGNING_KEY=/p" $KEYS >> wps.env
 
 sed -n "s/^JWK_1_PUB=/DCS_AUTH_KEY=/p" $KEYS > dcs.env
-
-sed -n 's/^TOKEN=\(.*\)/FIS_TOKEN="\1"/p' $KEYS > fis.env
-sed -n 's/^TOKEN_HASH=\(.*\)/FIS_TOKEN_HASHES=["\1"]/p' $KEYS >> fis.env
-sed -n "s/^C4GH_PRIV=/FIS_PRIVATE_KEY=/p" $KEYS >> fis.env
-
-sed -n "s/^C4GH_PUB=/GHGA_CONNECTOR_SERVER_PUBKEY=/p" $KEYS > tb.env
-sed -n "s/^C4GH_PUB=/TB_FILE_INGEST_PUBKEY=/p" $KEYS > tb.env
-
-sed -n "s/^JWK_1_PUB=/WPS_AUTH_KEY=/p" $KEYS > wps.env
-sed -n "s/^JWK_2_PRIV=/WPS_WORK_PACKAGE_SIGNING_KEY=/p" $KEYS >> wps.env
 
 echo "Container envs have been created."
