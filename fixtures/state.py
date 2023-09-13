bde4559fce4171ec0984a8fbb22bc4d1c0182dd7
Seyit Zor <seyitzor@gmail.com>
2023-09-13 12:57:33 +0000

    implement StateStorage base class

--- fixtures/state.py
+++ fixtures/state.py
@@ -17,6 +17,7 @@
 """Storage fixture for state management between test cases."""
 
 import re
+from abc import ABC, abstractmethod
 from typing import Any
 
 from hexkit.config import config_from_yaml
@@ -30,53 +31,94 @@ from fixtures.mongo import MongoFixture
 class StateConfig(BaseSettings):
     """Config for state storage"""
 
-    use_memory_storage: bool = True
+    use_memory_storage: bool = False
 
 
-class StateStorage:
-    """State storage object to store and manage state"""
+class StateStorage(ABC):
+    """Base class representing a storage for the state of the test bed
 
-    memory_storage: dict = {}
-    config: StateConfig
-    mongo: MongoFixture
+    Provides mechanisms for storing and managing the state"""
 
-    def __init__(self, config: StateConfig, mongo: MongoFixture):
-        self.config = config
+    @abstractmethod
+    def get_state(self, state_name: str) -> Any:
+        """Retrieve the state for a given key."""
+        ...
+
+    @abstractmethod
+    def set_state(self, state_name: str, value: Any) -> None:
+        """Set the state for a given key."""
+        ...
+
+    @abstractmethod
+    def unset_state(self, state_regex: str) -> None:
+        """Remove the state matching a regex string."""
+        ...
+
+    @abstractmethod
+    def reset_state(self) -> None:
+        """Reset the state storage to empty slate."""
+        ...
+
+
+class MemoryStateStorage(StateStorage):
+    """In-memory state storage implementing StateStorage"""
+
+    def __init__(self):
+        self.memory_storage = {}
+
+    def get_state(self, state_name: str) -> Any:
+        return self.memory_storage.get(state_name)
+
+    def set_state(self, state_name: str, value: Any):
+        self.memory_storage[state_name] = value
+
+    def unset_state(self, state_regex: str):
+        regex = re.compile(state_regex)
+        for state_name, _ in self.memory_storage.items():
+            if regex.match(state_name):
+                del self.memory_storage[state_name]
+
+    def reset_state(self):
+        self.memory_storage.clear()
+
+
+class MongoStateStorage(StateStorage):
+    """Mongo state storage implementing StateStorage"""
+
+    DB_NAME = "tb"
+    COLLECTION_NAME = "state"
+
+    def __init__(self, mongo: MongoFixture):
         self.mongo = mongo
 
     def get_state(self, state_name: str) -> Any:
-        if self.config.use_memory_storage:
-            return self.memory_storage.get(state_name, None)
-
-        state = self.mongo.find_document("tb", "state", {"_id": state_name})
+        state = self.mongo.find_document(
+            self.DB_NAME, self.COLLECTION_NAME, {"_id": state_name}
+        )
         return (state or {}).get("value")
 
     def set_state(self, state_name: str, value: Any):
-        if self.config.use_memory_storage:
-            self.memory_storage[state_name] = value
-        else:
-            self.mongo.replace_document(
-                "tb", "state", {"_id": state_name, "value": value}
-            )
+        self.mongo.replace_document(
+            self.DB_NAME, self.COLLECTION_NAME, {"_id": state_name, "value": value}
+        )
 
     def unset_state(self, state_regex: str):
-        if self.config.use_memory_storage:
-            regex = re.compile(state_regex)
-            for state_name in self.memory_storage:
-                if regex.match(state_name):
-                    self.memory_storage.pop(state_name)
-        else:
-            self.mongo.remove_document("tb", "state", {"_id": {"$regex": state_regex}})
+        self.mongo.remove_document(
+            self.DB_NAME, self.COLLECTION_NAME, {"_id": {"$regex": state_regex}}
+        )
 
     def reset_state(self):
-        if self.config.use_memory_storage:
-            self.memory_storage = {}
-        else:
-            self.mongo.empty_databases("tb")
+        self.mongo.empty_databases(self.DB_NAME)
 
 
 @fixture(name="state", scope="session")
 def state_fixture(mongo: MongoFixture) -> StateStorage:
     """Fixture that provides a state storage."""
     config = StateConfig()
-    return StateStorage(config=config, mongo=mongo)
+    storage = (
+        MemoryStateStorage()
+        if config.use_memory_storage
+        else MongoStateStorage(mongo=mongo)
+    )
+
+    return storage
